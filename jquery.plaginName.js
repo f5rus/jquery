3b871ccff21622153cf8542bb45467350e3f0fd2
Виталий <millionaire@mail.ru>
2014-10-10 15:47:36 +0700

    Update jquery.plaginName.js

--- jquery.plaginName.js
+++ jquery.plaginName.js
@@ -6,14 +6,20 @@ $(document).ready(function(){
 $.fn.defaultText = function(value){
   var element = this.eq(0);
   element.data('defaultText',value);
-  element.focus(function(){
-    if(element.val() == value){
-      element.val('').removeClass('defaultText');
-}
-}).blur(function(){
-if(element.val() == '' || element.val() == value){
-  element.addClass('defaultText').val(value);
-}
-});
-return element.blur();
+  element.focus(
+    function()
+    {
+      if(element.val() == value)
+      {
+        element.val('').removeClass('defaultText');
+      }
+  }
+  ).blur(function()
+    {
+      if(element.val() == '' || element.val() == value)
+      {
+        element.addClass('defaultText').val(value);
+      }
+    });
+  return element.blur();
 }
