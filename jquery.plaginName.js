4dd8a50a870b434458480c0045aceb0a772a6abc
Виталий <millionaire@mail.ru>
2014-10-10 15:45:37 +0700

    Update jquery.plaginName.js

--- jquery.plaginName.js
+++ jquery.plaginName.js
@@ -1,1 +1,19 @@
-d
+$(document).ready(function(){
+  $('#name').defaultText('Nickname');
+  $('#email').defaultText('Email (Gravatars are Enabled)');
+});
+// A custom jQuery method for placeholder text:
+$.fn.defaultText = function(value){
+  var element = this.eq(0);
+  element.data('defaultText',value);
+  element.focus(function(){
+    if(element.val() == value){
+      element.val('').removeClass('defaultText');
+}
+}).blur(function(){
+if(element.val() == '' || element.val() == value){
+  element.addClass('defaultText').val(value);
+}
+});
+return element.blur();
+}
